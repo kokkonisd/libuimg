1bb8257a5f36c140475fa69193f079ffebd18c9c
Dimitris Kokkonis <kokkonisd@gmail.com>
2020-12-26 22:17:32 +0200

    Fix RGB color issues for YUV444

--- tests/test_image_conversions.c
+++ tests/test_image_conversions.c
@@ -535,15 +535,15 @@ char * test_image_conversion_YUV444_to_RGB565 ()
     CUTS_ASSERT(img_rgb565->format == RGB565, "Converted RGB565 image has wrong format");
 
     // Calculate expected values
-    expected_r = yuv_to_rgb_r('Y', 'U', 'V');
-    expected_g = yuv_to_rgb_g('Y', 'U', 'V');
-    expected_b = yuv_to_rgb_b('Y', 'U', 'V');
+    expected_r = rescale_color(yuv_to_rgb_r('Y', 'U', 'V'), 0, 255, 0, 32);
+    expected_g = rescale_color(yuv_to_rgb_g('Y', 'U', 'V'), 0, 255, 0, 64);
+    expected_b = rescale_color(yuv_to_rgb_b('Y', 'U', 'V'), 0, 255, 0, 32);
 
     for (i = 0; i < width * height; i++) {
         // Get partial R, G, B
-        actual_r = img_rgb565->data[i * 2] & 0x1f;
+        actual_b = img_rgb565->data[i * 2] & 0x1f;
         actual_g = ((img_rgb565->data[i * 2] & 0xe0) >> 5) | ((img_rgb565->data[i * 2 + 1] & 0x07) << 3);
-        actual_b = (img_rgb565->data[i * 2 + 1] & 0xf8) >> 3;
+        actual_r = (img_rgb565->data[i * 2 + 1] & 0xf8) >> 3;
 
         CUTS_ASSERT(actual_r == (expected_r & 0x1f), "Wrong R value for RGB565 image on pixel %d", i);
         CUTS_ASSERT(actual_g == (expected_g & 0x3f), "Wrong G value for RGB565 image on pixel %d", i);
@@ -591,15 +591,15 @@ char * test_image_conversion_YUV444_to_RGB8 ()
     CUTS_ASSERT(img_rgb8->format == RGB8, "Converted RGB8 image has wrong format");
 
     // Calculate expected values
-    expected_r = yuv_to_rgb_r('Y', 'U', 'V');
-    expected_g = yuv_to_rgb_g('Y', 'U', 'V');
-    expected_b = yuv_to_rgb_b('Y', 'U', 'V');
+    expected_r = rescale_color(yuv_to_rgb_r('Y', 'U', 'V'), 0, 255, 0, 8);
+    expected_g = rescale_color(yuv_to_rgb_g('Y', 'U', 'V'), 0, 255, 0, 8);
+    expected_b = rescale_color(yuv_to_rgb_b('Y', 'U', 'V'), 0, 255, 0, 4);
 
     for (i = 0; i < width * height; i++) {
         // Get partial R, G, B
-        actual_r = img_rgb8->data[i] & 0x07;
-        actual_g = (img_rgb8->data[i] >> 3) & 0x7;
-        actual_b = (img_rgb8->data[i] >> 6) & 0x3;
+        actual_b = img_rgb8->data[i] & 0x03;
+        actual_g = (img_rgb8->data[i] >> 2) & 0x7;
+        actual_r = (img_rgb8->data[i] >> 5) & 0x7;
 
         CUTS_ASSERT(actual_r == (expected_r & 0x07), "Wrong R value for RGB8 image on pixel %d", i);
         CUTS_ASSERT(actual_g == (expected_g & 0x07), "Wrong G value for RGB8 image on pixel %d", i);
