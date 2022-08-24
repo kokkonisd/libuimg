cdf2f7f006057e46f7235806968945013fc1459e
Dimitris Kokkonis <kokkonisd@gmail.com>
2022-08-24 19:35:58 +0200

    Simplify inputs passed to AFL++
    There is no need to pass every type of format to AFL++; two different
    formats suffice (to show that the range of the pixels is not necessarily
    0-255).

--- fuzz/src/generate_images.c
+++ fuzz/src/generate_images.c
@@ -34,33 +34,13 @@ int main (void)
 {
     srand(time(NULL));
 
-    Image_t * yuv444 = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), YUV444);
-    Image_t * yuv444p = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), YUV444p);
     Image_t * yuv420p = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), YUV420p);
-    Image_t * rgb24 = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), RGB24);
-    Image_t * rgb565 = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), RGB565);
-    Image_t * rgb8 = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), RGB8);
-    Image_t * grayscale = generate_image(
-        RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), GRAYSCALE
-    );
     Image_t * ascii = generate_image(RAND_RANGE(MIN_WIDTH, MAX_WIDTH), RAND_RANGE(MIN_HEIGHT, MAX_HEIGHT), ASCII);
 
-    save_image(yuv444, "in/yuv444.img");
-    save_image(yuv444p, "in/yuv444p.img");
     save_image(yuv420p, "in/yuv420p.img");
-    save_image(rgb24, "in/rgb24.img");
-    save_image(rgb565, "in/rgb565.img");
-    save_image(rgb8, "in/rgb8.img");
-    save_image(grayscale, "in/grayscale.img");
     save_image(ascii, "in/ascii.img");
 
-    destroy_image(yuv444);
-    destroy_image(yuv444p);
     destroy_image(yuv420p);
-    destroy_image(rgb24);
-    destroy_image(rgb565);
-    destroy_image(rgb8);
-    destroy_image(grayscale);
     destroy_image(ascii);
 
     return 0;
